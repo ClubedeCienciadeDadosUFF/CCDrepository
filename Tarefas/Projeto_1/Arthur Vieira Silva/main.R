fe5e70ae26ced4ee2df9757b29e019a1b21d899c
Arthur Vieira Silva <arthurvieirasilva@gmail.com>
2015-04-09 20:12:01 -0300

    Add modularity

--- Tarefas/Projeto_1/Arthur Vieira Silva/main.R
+++ Tarefas/Projeto_1/Arthur Vieira Silva/main.R
@@ -1,33 +1,37 @@
 library('rvest')
 library('stringr')
-robbed_page <- html("http://www.ondefuiroubado.com.br/denuncias/44783/assaltada-voltando-do-mercado")
 
-title <- robbed_page %>%
-  html_nodes("section.sd-info-denuncia h1") %>%
-  html_text()
-title
+get_data <- function(page){
+  robbed_page <- html(page)
+  
+  title <- robbed_page %>%
+    html_nodes("section.sd-info-denuncia h1") %>%
+    html_text()
+  title
+  
+  objects <- robbed_page %>%
+    html_nodes("section.sd-objects div div.sd-objects-list") %>%
+    html_text();
+  exp <- gregexpr("([A-Z][A-Za-zÀ-Ûà-û ]*)",objects, perl=TRUE )
+  objects <- regmatches(objects, exp)[[1]]
+  
+  type <- robbed_page %>%
+    html_nodes("section.sd-info-denuncia h2.sd-info-type") %>%
+    html_text()
+  type <- str_trim(type)
+  
+  date_time <- robbed_page %>%
+    html_nodes("section.sd-info-denuncia span.sd-info-data-hora") %>%
+    html_text()
+  date_time <- strsplit(str_trim(date_time), " ")[[1]]
+  
+  description <- robbed_page %>%
+    html_nodes("section.sd-info-denuncia div.sd-info-desc") %>%
+    html_text()
+  description <- str_trim(description)
+  
+  text <- paste(title, type, date_time[1], date_time[2], paste(objects, collapse=","), sep=";")
+}
 
-objects <- robbed_page %>%
-  html_nodes("section.sd-objects div div.sd-objects-list") %>%
-  html_text();
-exp <- gregexpr("([A-Z][A-Za-zÀ-Ûà-û ]*)",objects, perl=TRUE )
-objects <- regmatches(objects, exp)
-objects
-
-type <- robbed_page %>%
-  html_nodes("section.sd-info-denuncia h2.sd-info-type") %>%
-  html_text()
-type <- str_trim(type)
-
-date_time <- robbed_page %>%
-  html_nodes("section.sd-info-denuncia span.sd-info-data-hora") %>%
-  html_text()
-date_time <- strsplit(str_trim(date_time), " ")[[1]]
-
-description <- robbed_page %>%
-  html_nodes("section.sd-info-denuncia div.sd-info-desc") %>%
-  html_text()
-description <- str_trim(description)
-
-text <- paste(title, type, date_time[1], date_time[2], sep=";")
-#data <- read.table(col.names=c("title","type", "date", "time", "objects"), )
+text <- get_data("http://www.ondefuiroubado.com.br/denuncias/44783/assaltada-voltando-do-mercado")
+data <- read.table(col.names=c("title","type", "date", "time", "objects"), text=text, sep=';')
