1cf20a3ff537be588b6f139e0858ede39b0e0064
Paulo Roberto Mann Marques Júnior <paulomann@id.uff.br>
2015-05-15 09:28:42 -0300

    Atualização do script
    A formação do sql insert está correta, mas por algum motivo tenho obtido
    erros de encode. Quando vou enviar a query, tenho o seguinte erro :
    ERROR:  invalid byte sequence for encoding "UTF8": 0xe3 0x6f 0x20.
    Descobri que pode ser que tenha caracteres nulos na string, ainda não
    consegui resolver.

--- Tarefas/Projeto_1/Paulo Roberto Mann Marques Junior/populate_BD.R
+++ Tarefas/Projeto_1/Paulo Roberto Mann Marques Junior/populate_BD.R
@@ -14,37 +14,47 @@ populate <- function()
     setwd(paste(DEFAULT_PATH, OCCURRENCES_PATH, sep="/"))
     NEW_PATH <- getwd()
     
-    library(RMySQL)
+    library(RPostgreSQL)
+    library(stringr)
     library(gdata)
-    mydb = dbConnect(MySQL(), user="root", password="qwer1234", dbname = "ondefuiroubado", host="localhost")
-    dbListTables(mydb)
-    
+    #options(encoding = "UTF-8")
+    drv <- dbDriver('PostgreSQL')
+    con <- dbConnect(drv, dbname='ondefuiroubado', port='5432', user='postgres', password='qwer1234')
     data <- data.frame()
     files <- list.files(path = NEW_PATH, pattern = "*.csv")
     
-    for(i in 1:1){
-      data <- read.csv(files[i])
-      cnames <- colnames(data)
-      query <- "INSERT INTO occurrence(idOccurrence, latitude, longitude, city, type, title,object_1, object_2, object_3, object_4, object_5, object_6, object_7, object_8,object_9, object_10, object_11, object_12, object_13, object_14, object_15, object_16,object_17, object_18, object_19, date_time, description) VALUES("
-      queries <- character()
-      for(j in 2:length(cnames)){
-        if(j >= 8 && j <= 26){
-          if(data[i,j] == "TRUE")
-            query <- paste(query, 1, ",", sep="")
+    for(v in 1:length(files)){
+      data <- read.csv(files[v], strip.white = TRUE)
+      for(i in 1:nrow(data)){
+        cnames <- colnames(data)
+        query <- "INSERT INTO occurrence(idoccurrence, latitude, longitude, city, type, title, object_1, object_2, object_3, object_4, object_5, object_6, object_7, object_8, object_9, object_10, object_11, object_12, object_13, object_14, object_15, object_16,object_17, object_18, object_19, date_time, description) VALUES("
+        queries <- character()
+        for(j in 2:length(cnames)){
+          if(j == 27) {
+            date_time <- as.character(data[i,j])
+            date_time <- trim(date_time)
+            time <- strsplit(date_time, split=" ")[[1]][2]
+            date <- as.Date(strsplit(date_time, split=" ")[[1]][1], "%d/%m/%Y")
+            query <- paste(query, '\'', paste(date,time,sep=" "), '\'' ,",", sep="")
+          }
+          else if(is.numeric(data[i,j]))
+            query <- paste(query, data[i,j], ",", sep="")
           else
-            query <- paste(query, 0, ",", sep="")
+          {
+            if(j != 28)
+              query <- paste(query, '\'', trim(data[i,j]), '\'', ",", sep="")
+            else
+              if(j == 28)
+                query <- paste(query, '\'', trim(data[i,j]), '\'', sep="")
+          }
         }
-        else if(j == 27)
-          query <- paste(query, '\'', as.POSIXct(data[i,j], "%Y-%m-%d %H:%M:%S"), '\'' ,",", sep="") #Do not work.
-        else if(is.numeric(data[i,j]))
-          query <- paste(query, data[i,j], ",", sep="")
-        else
-          query <- paste(query, '\'', trim(data[i,j]), '\'', ",", sep="")
+        query <- paste(query, ");", sep="")
+        print(query)
+        rs <- dbSendQuery(con, query)
+        print(fetch(rs,n=-1))
       }
-      query <- paste(query, ");", sep="")
-      cbind(queries, query)
-      print(query)
     }
-    lapply(dbListConnections(MySQL()), dbDisconnect)
+    lapply(dbListConnections(drv), dbDisconnect)
+    dbUnloadDriver(drv)
     setwd(DEFAULT_PATH)
 }
