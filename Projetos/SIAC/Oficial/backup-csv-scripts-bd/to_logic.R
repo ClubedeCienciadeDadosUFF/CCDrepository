4eb31f9ede8369f2f99204a2989fff00ed150a5f
Paulo Roberto Mann Marques Júnior <paulomann@id.uff.br>
2016-02-01 10:00:40 -0200

    Adicionando o to_logic tratando ocorrências sem nearby_location

--- Projetos/SIAC/Oficial/backup-csv-scripts-bd/to_logic.R
+++ Projetos/SIAC/Oficial/backup-csv-scripts-bd/to_logic.R
@@ -1,16 +1,20 @@
 normalize <- function(input) {
-  subf <- c(" ", "á", "é", "í", "ó", "ú", "â", "ê", "ô", "'", "ç", "ã")
-  subt <- c("_", "a", "e", "i", "o", "u", "a", "e", "o", "_", "c", "a")
-  input <- tolower(input)
-  for (i in 1:length(subf)) {
-    input <- gsub(subf[i], subt[i], input)
-  }
-  
-  return(input)
+#   subf <- c(" ", "á", "é", "í", "ó", "ú", "â", "ê", "ô", "'", "ç", "à")
+#   subt <- c("_", "a", "e", "i", "o", "u", "a", "e", "o", "_", "c", "a")
+#   unwanted_array = list(    'S'='S', 's'='s', 'Z'='Z', 'z'='z', 'À'='A', 'Á'='A', 'Â'='A', 'Ã'='A', 'Ä'='A', 'Å'='A', 'Æ'='A', 'Ç'='C', 'È'='E', 'É'='E',
+#                             'Ê'='E', 'Ë'='E', 'Ì'='I', 'Í'='I', 'Î'='I', 'Ï'='I', 'Ñ'='N', 'Ò'='O', 'Ó'='O', 'Ô'='O', 'Õ'='O', 'Ö'='O', 'Ø'='O', 'Ù'='U',
+#                             'Ú'='U', 'Û'='U', 'Ü'='U', 'Ý'='Y', 'Þ'='B', 'ß'='Ss', 'à'='a', 'á'='a', 'â'='a', 'ã'='a', 'ä'='a', 'å'='a', 'æ'='a', 'ç'='c',
+#                             'è'='e', 'é'='e', 'ê'='e', 'ë'='e', 'ì'='i', 'í'='i', 'î'='i', 'ï'='i', 'ð'='o', 'ñ'='n', 'ò'='o', 'ó'='o', 'ô'='o', 'õ'='o',
+#                             'ö'='o', 'ø'='o', 'ù'='u', 'ú'='u', 'û'='u', 'ý'='y', 'ý'='y', 'þ'='b', 'ÿ'='y' )
+    input <- tolower(input)
+#   for(i in seq_along(unwanted_array))
+#     input <- gsub(names(unwanted_array)[i],unwanted_array[i],input)
+#   
+    return(input)
 }
 
 to_logic <- function(FILE_NAME = "time_discretize_suburb_updated.csv") {
-  dataset <- read.csv(FILE_NAME)
+  dataset <- read.csv(FILE_NAME) #header = TRUE, encoding = "utf8")
   last_occ = -1
   last_sub = "-1"
   obj <- c("carteira", "bolsa_ou_mochila", "celular", "documentos",
@@ -25,10 +29,10 @@ to_logic <- function(FILE_NAME = "time_discretize_suburb_updated.csv") {
       write(paste("occurrence(",
                   dataset$n_occurrence[i],
                   ",",
-                  normalize(substr(dataset$occurrence_type[i],
-                                   1, nchar(as.vector(dataset$occurrence_type[i]))-1)),
+                  normalize(substr(dataset$occurrence_type[i], 1, nchar(as.vector(dataset$occurrence_type[i]))-1)),
+                  #normalize(dataset$occurrence_type[i]),
                   ").", sep = ""),
-            "prep.f", append = TRUE, sep = "\n")
+            "siac.f", append = TRUE, sep = "\n")
       
       #.b   
       #geo_position
@@ -37,40 +41,40 @@ to_logic <- function(FILE_NAME = "time_discretize_suburb_updated.csv") {
                   ",",
                   normalize(dataset$suburb[i]),
                   ").", sep = ""),
-            "arg.b", append = TRUE, sep = "\n")
+            "siac.b", append = TRUE, sep = "\n")
       #date
       if(dataset$date[i] == "business_day") {
         write(paste("business_day(",
                     last_occ,
                     ").", sep = ""),
-              "arg.b", append = TRUE, sep = "\n")
+              "siac.b", append = TRUE, sep = "\n")
       } else {
         write(paste("weekend(",
                     last_occ,
                     ").", sep = ""),
-              "arg.b", append = TRUE, sep = "\n")
+              "siac.b", append = TRUE, sep = "\n")
       }
       #time
       if(dataset$time[i] == "morning") {
         write(paste("morning(",
                     last_occ,
                     ").", sep = ""),
-              "arg.b", append = TRUE, sep = "\n")
+              "siac.b", append = TRUE, sep = "\n")
       } else if(dataset$time[i] == "dawn") {
         write(paste("dawn(",
                     last_occ,
                     ").", sep = ""),
-              "arg.b", append = TRUE, sep = "\n")
+              "siac.b", append = TRUE, sep = "\n")
       } else if(dataset$time[i] == "night") {
         write(paste("night(",
                     last_occ,
                     ").", sep = ""),
-              "arg.b", append = TRUE, sep = "\n")
+              "siac.b", append = TRUE, sep = "\n")
       } else {
         write(paste("evening(",
                     last_occ,
                     ").", sep = ""),
-              "arg.b", append = TRUE, sep = "\n")
+              "siac.b", append = TRUE, sep = "\n")
       }
       #obj
       for(j in obj) {
@@ -80,25 +84,28 @@ to_logic <- function(FILE_NAME = "time_discretize_suburb_updated.csv") {
                       ",",
                       j,
                       ").", sep = ""),
-                "arg.b", append = TRUE, sep = "\n")
+                "siac.b", append = TRUE, sep = "\n")
         }
       }
     }
     #.b
     #nearby_location
-    write(paste("nearby_location(",
-                last_occ,
-                ",",
-                normalize(dataset$nearby_location[i]),
-                ").", sep = ""),
-          "arg.b", append = TRUE, sep = "\n")
-    #in
-    write(paste("in(",
-                normalize(dataset$nearby_location[i]),
-                ",",
-                normalize(last_sub),
-                ").", sep = ""),
-          "arg.b", append = TRUE, sep = "\n")
+    if(dataset$nearby_location[i] != "")
+    {
+      write(paste("nearby_location(",
+                  last_occ,
+                  ",",
+                  normalize(dataset$nearby_location[i]),
+                  ").", sep = ""),
+            "siac.b", append = TRUE, sep = "\n")
+      #in
+      write(paste("in(",
+                  normalize(dataset$nearby_location[i]),
+                  ",",
+                  normalize(last_sub),
+                  ").", sep = ""),
+            "siac.b", append = TRUE, sep = "\n")
+    }
   }
 }
 
@@ -117,7 +124,7 @@ build_negative <- function(FILE_NAME = "time_discretize_suburb_updated.csv") {
                     ",",
                     nc,
                     ").", sep = ""),
-              "a.n", append = TRUE, sep = "\n")
+              "siac.n", append = TRUE, sep = "\n")
       }
     }
   }
