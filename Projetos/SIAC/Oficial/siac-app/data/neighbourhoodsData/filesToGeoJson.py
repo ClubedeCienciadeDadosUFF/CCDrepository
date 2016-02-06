51cadbe80402244b3ef3c018feef45c8fe8f01cc
Paulo Mann <paulomann@id.uff.br>
2016-02-06 15:10:42 -0200

    Agora o mapa de induções mostra quantas ocorrências existem dentro do bairro

--- Projetos/SIAC/Oficial/siac-app/data/neighbourhoodsData/filesToGeoJson.py
+++ Projetos/SIAC/Oficial/siac-app/data/neighbourhoodsData/filesToGeoJson.py
@@ -2,13 +2,47 @@ from sys import argv
 import json
 import re
 from pprint import pprint
+import unicodedata
+import string
 
-fileNames = argv[1:]
+# Args : 1 - algorithm , 2 - folder/* (todos os json dos bairros)
+# 2 - niteroi-occurrences.json(Todas as ocorrências com o bairro incluso
+# para poder calcular a densidade)
+fileNames = argv[2:]
+args = argv[:2]
 
 print(fileNames)
+print(args)
 
 finalGeoJSON = {'type': 'FeatureCollection'}
 finalGeoJSON['features'] = []
+
+occurrences = {}
+
+with open(args[1]) as occurrencesFile:
+    occurrences = json.load(occurrencesFile)
+
+
+def remove_accents(data):
+    return ''.join(x for x in unicodedata.normalize('NFKD', data) if x in string.ascii_letters).lower()
+
+
+def calculateDensity(neighbourhood):
+    density = 0
+    for occ in occurrences:
+        suburb = occ['suburb']
+        suburb = suburb.split(" ")
+        catSuburb = ""
+        if not (len(suburb) == 1):
+            catSuburb = catSuburb + remove_accents(suburb[0])
+            for sub in suburb[1:]:
+                catSuburb = catSuburb + "_" + remove_accents(sub)
+        else:
+            catSuburb = remove_accents(suburb[0])
+        print(catSuburb)
+        if neighbourhood == catSuburb:
+            density = density + 1
+    return density
 
 
 def getGeoJSON(data, fileName):
@@ -16,7 +50,8 @@ def getGeoJSON(data, fileName):
     name = matchObj.group(1)
     geojson = {}
     geojson['type'] = "Feature"
-    geojson['properties'] = {"name": name}
+    density = calculateDensity(name)
+    geojson['properties'] = {"name": name, "density":density}
     geojson['geometry'] = {"type": "Polygon"}
     geojson['geometry']['coordinates'] = []
 
@@ -28,8 +63,8 @@ def getGeoJSON(data, fileName):
         geographicPoint = [point['lng'], point['lat']]
         geoPoints.append(geographicPoint)
 
-   
-    geoPoints.append([firstGeographicPoint['lng'], firstGeographicPoint['lat']])
+    geoPoints.append(
+        [firstGeographicPoint['lng'], firstGeographicPoint['lat']])
 
     geojson['geometry']['coordinates'].append(geoPoints)
     # pprint(geojson)
@@ -45,3 +80,5 @@ for fileName in fileNames:
 
 with open('niteroi-neighbourhoods.geojson', 'w') as outfile:
     json.dump(finalGeoJSON, outfile)
+
+occurrencesFile.close()
